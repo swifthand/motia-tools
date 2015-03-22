90188e3323127adf7e8c23050c696a15790e3f2b
Paul Kwiatkowski <paul@groupraise.com>
2015-03-21 22:19:42 -0500

    Renamed csv_merge to merge_example, added flag example.

--- merge_example.rb
+++ merge_example.rb
@@ -1,22 +1,22 @@
 load 'environment.rb'
 
 ## Example: Merging csv data as a list of rows into a single template.
-def test_single
+def run_test_single
   template    = 'test/test_single_template.html.erb'
   data_source = 'test/test_single_data.csv'
-  output_file = 'test/test_single_output.html'
+  output_file = 'test/test_single_output_flag.html'
   merge       = TemplateMergeSingle.new(template, data_source)
   merge.render_to_file(output_file)
 end
 
 ## Example: Merging each row in a csv with a template as its own file.
-def test_multiple
+def run_test_multiple
   template    = 'test/test_multiple_template.html.erb'
   data_source = 'test/test_multiple_data.csv'
   merge       = TemplateMergeMultiple.new(template, data_source)
   merge.render_to_file do |row, row_number|
-    "test/test_multiple_output_line_#{row_number}.html"
+    "test/#{row.country}.html"
   end
 end
 
-test_multiple
+run_test_multiple
